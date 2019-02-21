82c29805cf64150088435e7b4f2a85b7356cb42f
Damandeep Singh Bawa <41473923+DamFlancoz@users.noreply.github.com>
2019-02-20 17:29:22 -0800

    Final EvalTable
    Added final Eval Table

--- PythonLibs/EvalTable.py
+++ PythonLibs/EvalTable.py
@@ -21,17 +21,17 @@ class NotFit(Exception):
 Converts time from webscraper to tuple of start and end integers.
 eg. '8:30 am - 9:50 am' = (8.5, 10)
 '''
-def getTime(section):
-    time = section['time'].split(' - ') #[['3:30 pm','4:30 pm']
+def getTime(time):
+    time = time.split(' - ') #[['3:30 pm','4:30 pm']
 
-    for i in range(len(time)):
+    for i in range(2): # for start and end term
         t = time[i][:5].split(':') #takes'3:30 ' or '10:50'
         t[0] = int(t[0].strip())
 
         if '30' in t[1].strip() or '20' in t[1].strip(): t[0] += 0.5
         elif '50' in t[1].strip() : t[0] += 1
         
-        if 'pm' in time[i] and t[0] !=12:t[0]+=12
+        if 'pm' in time[i] and t[0] != 12 :t[0]+=12
 
         time[i] = t[0]
     return tuple(time)
@@ -48,23 +48,23 @@ def insertInTable(course,section,day,time,table):
 
     #inserting in free day
     if table[days[day]]==[]:
-        table[days[day]].insert(0,[time[0],time[1],course[0]+course[1],section['section']])
+        table[days[day]].insert(0,[time[0],time[1],course[0]+course[1],section])
 
     #inserting at start of day
-    elif time[1] <= table[days[day]][0][0]:  #time=[start,end]
-        table[days[day]].insert(0,[time[0],time[1],course[0]+course[1],section['section']])
+    elif table[days[day]][0][0] >= time[1]:
+        table[days[day]].insert(0,[time[0],time[1],course[0]+course[1],section])
         
     else:
         #inserting during the day
         for i in range(len(table[days[day]])-1):
-            if table[days[day]][i][1] <= time[0] and time[1] <= table[days[day]][i+1][0]:
-                table[days[day]].insert(i+1,[time[0],time[1],course[0]+course[1],section['section']])
+            if table[days[day]][i][1] <= time[0] and table[days[day]][i+1][0] >= time[1]:
+                table[days[day]].insert(i+1,[time[0],time[1],course[0]+course[1],section])
                 break
 
         #inserting at end of day
         else:
             if table[days[day]][-1][1] <= time[0]:
-                table[days[day]].append([time[0],time[1],course[0]+course[1],section['section']])
+                table[days[day]].append([time[0],time[1],course[0]+course[1],section])
             else:
                 raise NotFit()
     return table
@@ -91,17 +91,17 @@ def evalTable(selectedCourses,coursesInfo,tables):
     for course in selectedCourses:
         for Type in coursesInfo[course[0]+course[1]]:
             for section in coursesInfo[course[0]+course[1]][Type]:
-                for i,t in enumerate(tables):
+                 for i,t in enumerate(tables):
 
-                    #[start,end] eg [11,13.5] for'11am-1:30pm'
-                    time = getTime(section)
+                    #[start,end] eg [11,13.5] for'11am-1:20pm'
+                    time = getTime(section['time'])
 
                     if not (time,section['days'],i) in newTables:
                         table = deepcopy(t)
 
                         try:
                             for day in section['days']:
-                                table = insertInTable(course,section,day,time,table)
+                                table = insertInTable(course,section['section'],day,time,table)
 
                             newTables[(time,section['days'],i)]= table
                             
@@ -120,24 +120,130 @@ def evalTable(selectedCourses,coursesInfo,tables):
 
     return tables
 
+'''
+Tests if function performs its intended use
+
+TableEval uses dictionaries so there is a chance that it will provide
+same tables in different orders. Must be kept in mind when using different
+system or IDE
+'''
+
 def test_evalTable():
-    coursesInfo = {'MATH200': {'labs': [], 'lectures': [{'crn': '22034', 'section': 'A01', 'days': 'MR', 'place': 'Hickman Building 105', 'time': '8:30 am - 11:50 pm', 'instructor': 'Andrew   McEachern (P)', 'type': 'Lecture'}], 'tutorials': [{'crn': '22035', 'section': 'T01', 'days': 'T', 'place': 'Clearihue Building A212', 'time': '3:30 pm - 4:20 pm', 'instructor': 'TBA', 'type': 'Tutorial'}, {'crn': '22036', 'section': 'T02', 'days': 'T', 'place': 'Cornett Building A121', 'time': '3:30 pm - 4:20 pm', 'instructor': 'TBA', 'type': 'Tutorial'}]}, 'MATH204': {'labs': [], 'lectures': [{'crn': '22040', 'section': 'A01', 'days': 'TWF', 'place': 'David Strong Building C103', 'time': '1:30 pm - 2:20 pm', 'instructor': 'Muhammad   Awais (P)', 'type': 'Lecture'}, {'crn': '22041', 'section': 'A02', 'days': 'TWF', 'place': 'David Turpin Building A104', 'time': '8:30 am - 9:20 am', 'instructor': 'Slim   Ibrahim (P)', 'type': 'Lecture'}], 'tutorials': [{'crn': '22042', 'section': 'T01', 'days': 'F', 'place': 'David Turpin Building A110', 'time': '3:30 pm - 4:20 pm', 'instructor': 'TBA', 'type': 'Tutorial'}, {'crn': '22043', 'section': 'T02', 'days': 'R', 'place': 'David Turpin Building A102', 'time': '4:30 pm - 5:20 pm', 'instructor': 'TBA', 'type': 'Tutorial'}, {'crn': '22044', 'section': 'T03', 'days': 'F', 'place': 'David Turpin Building A102', 'time': '2:30 pm - 3:20 pm', 'instructor': 'TBA', 'type': 'Tutorial'}, {'crn': '22045', 'section': 'T04', 'days': 'F', 'place': 'David Turpin Building A102', 'time': '3:30 pm - 4:20 pm', 'instructor': 'TBA', 'type': 'Tutorial'}]}}
+    coursesInfo = {'MATH200': {'labs': [],
+  'lectures': [{'type': 'Lecture',
+    'section': 'A01',
+    'days': 'MR',
+    'place': 'Hickman Building 105',
+    'time': '8:30 am - 11:50 pm',
+    'instructor': 'Andrew   McEachern (P)',
+    'crn': '22034'}],
+  'tutorials': [{'type': 'Tutorial',
+    'section': 'T01',
+    'days': 'T',
+    'place': 'Clearihue Building A212',
+    'time': '3:30 pm - 4:20 pm',
+    'instructor': 'TBA',
+    'crn': '22035'},
+   {'type': 'Tutorial',
+    'section': 'T02',
+    'days': 'T',
+    'place': 'Cornett Building A121',
+    'time': '3:30 pm - 4:20 pm',
+    'instructor': 'TBA',
+    'crn': '22036'}]},
+ 'MATH204': {'labs': [],
+  'lectures': [{'type': 'Lecture',
+    'section': 'A01',
+    'days': 'TWF',
+    'place': 'David Strong Building C103',
+    'time': '1:30 pm - 2:20 pm',
+    'instructor': 'Muhammad   Awais (P)',
+    'crn': '22040'},
+   {'type': 'Lecture',
+    'section': 'A02',
+    'days': 'TWF',
+    'place': 'David Turpin Building A104',
+    'time': '8:30 am - 9:20 am',
+    'instructor': 'Slim   Ibrahim (P)',
+    'crn': '22041'}],
+  'tutorials': [{'type': 'Tutorial',
+    'section': 'T01',
+    'days': 'F',
+    'place': 'David Turpin Building A110',
+    'time': '3:30 pm - 4:20 pm',
+    'instructor': 'TBA',
+    'crn': '22042'},
+   {'type': 'Tutorial',
+    'section': 'T02',
+    'days': 'R',
+    'place': 'David Turpin Building A102',
+    'time': '4:30 pm - 5:20 pm',
+    'instructor': 'TBA',
+    'crn': '22043'},
+   {'type': 'Tutorial',
+    'section': 'T03',
+    'days': 'F',
+    'place': 'David Turpin Building A102',
+    'time': '2:30 pm - 3:20 pm',
+    'instructor': 'TBA',
+    'crn': '22044'},
+   {'type': 'Tutorial',
+    'section': 'T04',
+    'days': 'F',
+    'place': 'David Turpin Building A102',
+    'time': '3:30 pm - 4:20 pm',
+    'instructor': 'TBA',
+    'crn': '22045'}]}}
+
     selectedCourses=[['MATH','200'],['MATH','204']]
     tables = [[[],[],[],[],[]]]
     
-    expected = [[[[8.5, 10, 'MATH200', 'A01']], [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH200', 'T01', 'T02']], [[13.5, 14.5, 'MATH204', 'A01']], [[8.5, 10, 'MATH200', 'A01'], [16.5, 17.5, 'MATH204', 'T02']], [[13.5, 14.5, 'MATH204', 'A01']]], [[[8.5, 10, 'MATH200', 'A01']], [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH200', 'T01', 'T02']], [[8.5, 9.5, 'MATH204', 'A02']], [[8.5, 10, 'MATH200', 'A01'], [16.5, 17.5, 'MATH204', 'T02']], [[8.5, 9.5, 'MATH204', 'A02']]], [[[8.5, 10, 'MATH200', 'A01']], [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH200', 'T01', 'T02']], [[13.5, 14.5, 'MATH204', 'A01']], [[8.5, 10, 'MATH200', 'A01']], [[13.5, 14.5, 'MATH204', 'A01'], [14.5, 15.5, 'MATH204', 'T03']]], [[[8.5, 10, 'MATH200', 'A01']], [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH200', 'T01', 'T02']], [[13.5, 14.5, 'MATH204', 'A01']], [[8.5, 10, 'MATH200', 'A01']], [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH204', 'T01', 'T04']]], [[[8.5, 10, 'MATH200', 'A01']], [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH200', 'T01', 'T02']], [[8.5, 9.5, 'MATH204', 'A02']], [[8.5, 10, 'MATH200', 'A01']], [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH204', 'T01', 'T04']]], [[[8.5, 10, 'MATH200', 'A01']], [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH200', 'T01', 'T02']], [[8.5, 9.5, 'MATH204', 'A02']], [[8.5, 10, 'MATH200', 'A01']], [[8.5, 9.5, 'MATH204', 'A02'], [14.5, 15.5, 'MATH204', 'T03']]]]
-    
+    expected = [[[[8.5, 12, 'MATH200', 'A01']],
+  [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH200', 'T01', 'T02']],
+  [[13.5, 14.5, 'MATH204', 'A01']],
+  [[8.5, 12, 'MATH200', 'A01'], [16.5, 17.5, 'MATH204', 'T02']],
+  [[13.5, 14.5, 'MATH204', 'A01']]],
+ [[[8.5, 12, 'MATH200', 'A01']],
+  [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH200', 'T01', 'T02']],
+  [[8.5, 9.5, 'MATH204', 'A02']],
+  [[8.5, 12, 'MATH200', 'A01'], [16.5, 17.5, 'MATH204', 'T02']],
+  [[8.5, 9.5, 'MATH204', 'A02']]],
+ [[[8.5, 12, 'MATH200', 'A01']],
+  [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH200', 'T01', 'T02']],
+  [[13.5, 14.5, 'MATH204', 'A01']],
+  [[8.5, 12, 'MATH200', 'A01']],
+  [[13.5, 14.5, 'MATH204', 'A01'], [14.5, 15.5, 'MATH204', 'T03']]],
+ [[[8.5, 12, 'MATH200', 'A01']],
+  [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH200', 'T01', 'T02']],
+  [[13.5, 14.5, 'MATH204', 'A01']],
+  [[8.5, 12, 'MATH200', 'A01']],
+  [[13.5, 14.5, 'MATH204', 'A01'], [15.5, 16.5, 'MATH204', 'T01', 'T04']]],
+ [[[8.5, 12, 'MATH200', 'A01']],
+  [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH200', 'T01', 'T02']],
+  [[8.5, 9.5, 'MATH204', 'A02']],
+  [[8.5, 12, 'MATH200', 'A01']],
+  [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH204', 'T01', 'T04']]],
+ [[[8.5, 12, 'MATH200', 'A01']],
+  [[8.5, 9.5, 'MATH204', 'A02'], [15.5, 16.5, 'MATH200', 'T01', 'T02']],
+  [[8.5, 9.5, 'MATH204', 'A02']],
+  [[8.5, 12, 'MATH200', 'A01']],
+  [[8.5, 9.5, 'MATH204', 'A02'], [14.5, 15.5, 'MATH204', 'T03']]]]
+
     tables = evalTable(selectedCourses,coursesInfo,tables)
 
-    if tables == expected:
-        print 'pass'
+    for i in expected:
+        if i not in tables:
+            break
+        else:
+            tables.remove(i) 
     else:
-        print 'Error detected'
+        if tables==[]: return 'pass'
 
-#lets you access tables made during process after completion in case of test
-tables=[]
+    return 'Error detected'
+        
+
 if __name__=='__main__':
-    test_evalTable()
+    print test_evalTable()
 
     
 
