b718905e843b3de4b6e2e6a7989d61c3299d9627
Damandeep Singh Bawa <41473923+DamFlancoz@users.noreply.github.com>
2019-04-29 17:26:02 -0700

    Converted to python3
    except in WebScrapper (urllib.requests vs urllib2) all code is python 2.7 compatible

--- PythonLibs/WebScrapper.py
+++ PythonLibs/WebScrapper.py
@@ -1,4 +1,4 @@
-from urllib2 import urlopen as uOpen
+from urllib.request import urlopen
 
 '''
 Takes: term code, Course Object with course name and course no.
@@ -9,7 +9,7 @@ def getPage(TERM,course):
     Data = 'term_in='+TERM+'&sel_subj=dummy&sel_day=dummy&sel_schd=dummy&sel_insm=dummy&sel_camp=dummy&sel_levl=dummy&sel_sess=dummy&sel_instr=dummy&sel_ptrm=dummy&sel_attr=dummy&sel_subj='+course.course+'&sel_crse='+course.num+'&sel_title=&sel_schd=%25&sel_insm=%25&sel_from_cred=&sel_to_cred=&sel_camp=%25&sel_levl=%25&sel_ptrm=%25&sel_instr=%25&begin_hh=0&begin_mi=0&begin_ap=a&end_hh=0&end_mi=0&end_ap=a'
     # Data contains COURSE and COURSENO further down
  
-    return uOpen(url=Url, data=Data).read()
+    return urlopen(url=Url, data=Data.encode()).read().decode()
 
 
 '''
@@ -17,7 +17,7 @@ Tests: getPage with CSC 111, 2019 Jan-Apr term
 Puts: out a.html
 '''
 def Test_getPage():
-    with open('a.html','w') as t:
+    with open('TestOutWebScrapper.html','w') as t:
         t.write(getPage('201901',Course('CSC','111')))
 
 
