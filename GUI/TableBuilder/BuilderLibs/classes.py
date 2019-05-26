3cc9c592aed5d08f5ff363b97fb2f966e8171595
Damandeep Singh Bawa <41473923+DamFlancoz@users.noreply.github.com>
2019-05-26 01:02:14 -0700

    completed style change

--- GUI/TableBuilder/BuilderLibs/classes.py
+++ GUI/TableBuilder/BuilderLibs/classes.py
@@ -2,8 +2,8 @@
 This module contains all the classes used.
 '''
 
-class Table:
 
+class Table:
     def __init__(self):
 
         self.M = []
@@ -15,17 +15,17 @@ class Table:
         # each element is a list of sections which have same time
         self.sections = []
 
-        #adds Course('start) and Course('end') to each day
-        #self.addPaddingCourses()
+        # adds Course('start) and Course('end') to each day
+        # self.addPaddingCourses()
 
-        #Course('break') may also be added
+        # Course('break') may also be added
 
-    def __getitem__(self,i):
+    def __getitem__(self, i):
 
         # allows t['M'] to access t.M
         # Used in inserting in tables
 
-        return eval('self.'+i)
+        return eval("self." + i)
 
     def __iter__(self):
 
@@ -35,150 +35,174 @@ class Table:
         yield self.R
         yield self.F
 
-    def __str__(self): #TODO: imporve this
+    def __str__(self):  # TODO: imporve this
 
         # course_rep eg. [13,14,'CSC111','A01']
-        return '\n'.join(day +': '+str(table)[1:-1] for day,table in zip('MTWRF',self))
+        return "\n".join(
+            day + ": " + str(table)[1:-1] for day, table in zip("MTWRF", self)
+        )
 
     def addPddingCourses(self):
-        #adds Course('start) and Course('end') to each day
+        # adds Course('start) and Course('end') to each day
 
         for day in self:
-            day+=[Course('start',''),Course('end','')]
-            day[0].time = (0,0)
-            day[1].time = (24,24)
+            day += [Course("start", ""), Course("end", "")]
+            day[0].time = (0, 0)
+            day[1].time = (24, 24)
+
 
 class Course:
-
-    def __init__(self,course,num):
+    def __init__(self, course, num):
 
         self.name = course
         self.num = num
 
-        #Contain Section objects
+        # Contain Section objects
         self.lectures = []
         self.labs = []
         self.tutorials = []
 
-    def __eq__(self,course):
+    def __eq__(self, course):
 
-        if (type(course) == list and len(course) == 2): #Used in main->rem Command, takes [course,num]
+        if (
+            type(course) == list and len(course) == 2
+        ):  # Used in main->rem Command, takes [course,num]
             return course[0] == self.name and course[1] == self.num
 
-        elif (type(course) == Course):
-            return (self.name == course.name
-                    and self.num == course.num
-                    and self.lectures == course.lectures
-                    and self.labs == course.labs
-                    and self.tutorials == course.tutorials)
-        else :
+        elif type(course) == Course:
+            return (
+                self.name == course.name
+                and self.num == course.num
+                and self.lectures == course.lectures
+                and self.labs == course.labs
+                and self.tutorials == course.tutorials
+            )
+        else:
             return NotImplemented
 
-
-    def __ne__(self,course): #Used in main-> rem Command, takes [course,num]
+    def __ne__(self, course):  # Used in main-> rem Command, takes [course,num]
         return not self == course
 
     def __str__(self):
-        s = 'Course: ' + self.name + ' ' + self.num + '\nLectures:\n'
+        s = "Course: " + self.name + " " + self.num + "\nLectures:\n"
 
         for i in self.lectures:
-            s+='\t'+str(i)+'\n'
+            s += "\t" + str(i) + "\n"
 
-        s+='Labs:\n'
+        s += "Labs:\n"
 
         for i in self.labs:
-            s+='\t'+str(i)+'\n'
+            s += "\t" + str(i) + "\n"
 
-
-        s+='Tutorials:\n'
+        s += "Tutorials:\n"
 
         for i in self.tutorials:
-            s+='\t'+str(i)+'\n'
+            s += "\t" + str(i) + "\n"
 
         return s
 
-    def __iter__(self): # type - lectures, labs & tutorials
+    def __iter__(self):  # type - lectures, labs & tutorials
         yield self.lectures
         yield self.labs
         yield self.tutorials
 
 
-
-
 class Section:
-
     def __init__(self):
 
-        self.cName = ''
-        self.cNum = ''
-        self.section = ''
-        self.crn = ''
+        self.cName = ""
+        self.cNum = ""
+        self.section = ""
+        self.crn = ""
         self.time = ()
-        self.days = ''
-        self.place = ''
-        self.instructor = ''
+        self.days = ""
+        self.place = ""
+        self.instructor = ""
 
-    def __eq__(self,other):
+    def __eq__(self, other):
 
         # Used in adding sections with same timings to table in eval table and in its test
         # Gives true if of same timmings nad course, doesnot need to be exactly same.
         # eg: T01 and T02 can be equal.
-        if ((type(other) == tuple or type(other) == list) and len(other) == 4):
-            return (other[0] == self.time[0] and other[1] == self.time[1]
-                    and other[2] == self.cName+self.cNum)
+        if (type(other) == tuple or type(other) == list) and len(other) == 4:
+            return (
+                other[0] == self.time[0]
+                and other[1] == self.time[1]
+                and other[2] == self.cName + self.cNum
+            )
 
-        elif (type(other) == Section):
-            return (other.time == self.time
-                    and other.days == self.days
-                    and other.cName == self.cName
-                    and other.cNum == self.cNum)
+        elif type(other) == Section:
+            return (
+                other.time == self.time
+                and other.days == self.days
+                and other.cName == self.cName
+                and other.cNum == self.cNum
+            )
 
         else:
             return NotImplemented
 
-    def __ne__(self,other):
-        return (not self == other)
+    def __ne__(self, other):
+        return not self == other
 
     def __str__(self):
-        return ('Course: ' + self.cName + ' ' + self.cNum
-                + ' | Section: ' + self.section
-                + ' | Time: ' + str(self.time)
-                + ' | Days: ' + self.days
-                )
+        return (
+            "Course: "
+            + self.cName
+            + " "
+            + self.cNum
+            + " | Section: "
+            + self.section
+            + " | Time: "
+            + str(self.time)
+            + " | Days: "
+            + self.days
+        )
 
     def __rep__(self):
-        return('Course: ' + self.cName + ' ' + self.cNum
-                + ' | Section: ' + self.section
-                + ' | Time: ' + str(self.time)
-                + ' | Days: ' + self.days
-                )
+        return (
+            "Course: "
+            + self.cName
+            + " "
+            + self.cNum
+            + " | Section: "
+            + self.section
+            + " | Time: "
+            + str(self.time)
+            + " | Days: "
+            + self.days
+        )
 
-    def setTime(self,time):
+    def setTime(self, time):
 
         '''
         Converts time from webscraper to tuple of start and end integers.
         eg. '8:30 am - 9:50 am' = (8.5, 10)
         '''
 
-        time = time.split(' - ')  #[['3:30 pm','4:30 pm']
+        time = time.split(" - ")  # [['3:30 pm','4:30 pm']
 
         # For start('3:30 pm') and end ('4:30 pm') terms
         for i in range(2):
 
-            t = time[i][:5].split(':') #converts to '3:30 ' or '10:50'
-            t[0] = int(t[0].strip())   #takes hour eg. 3 or 10
+            t = time[i][:5].split(":")  # converts to '3:30 ' or '10:50'
+            t[0] = int(t[0].strip())  # takes hour eg. 3 or 10
 
             # Adjust to 24 hr notation
-            if 'pm' in time[i] and t[0] != 12 :t[0]+=12
+            if "pm" in time[i] and t[0] != 12:
+                t[0] += 12
 
             # Adjust according to minutes eg. 3:30/3:20 to 3.5
-            if '30' in t[1].strip() or '20' in t[1].strip(): t[0] += 0.5
-            elif '50' in t[1].strip() : t[0] += 1
+            if "30" in t[1].strip() or "20" in t[1].strip():
+                t[0] += 0.5
+            elif "50" in t[1].strip():
+                t[0] += 1
 
             # Store new value
             time[i] = t[0]
 
         self.time = tuple(time)
+
 
 # Exceptions
 
@@ -186,11 +210,12 @@ class Section:
 Raised if the Uvic says sections for course are not available.
 Used in WebScrapper
 '''
-class NoSectionsAvailableOnline(Exception):
 
-    def __init__(self,course):
-        super().__init__('')
-        self.course=course
+
+class NoSectionsAvailableOnline(Exception):
+    def __init__(self, course):
+        super().__init__("")
+        self.course = course
 
 
 '''
@@ -198,16 +223,19 @@ raised if no sections of a type can fit due to inputed time constraints
 eg. you cannot enter any tutroial of Engr 141 because they are all at night
     and you said you dont want any night stuff.
 '''
-class NoSectionOfTypeFit(Exception):
 
-    def __init__(self,course):
-        super().__init__('')
-        self.course=course
+
+class NoSectionOfTypeFit(Exception):
+    def __init__(self, course):
+        super().__init__("")
+        self.course = course
 
 
 '''
 Raised if a section doesnot fit in a timetable
 Used in EvalTable. Not an error.
 '''
+
+
 class NotFit(Exception):
     pass
--- GUI/TableBuilder/BuilderLibs/tablebuilder.py
+++ GUI/TableBuilder/BuilderLibs/tablebuilder.py
@@ -1,58 +1,60 @@
 '''
 This module provides functionality to calculate time tables.
+Use eval_table.
 '''
 
-from copy import deepcopy # For testing eval table
-from time import localtime # for seting term
+from copy import deepcopy  # For testing eval table
+from time import localtime  # for seting term
 
-from .classes import Table,Course,Section,NotFit,NoSectionOfTypeFit
-
-
-
-term = '201901'
+from .classes import Table, Course, Section, NotFit, NoSectionOfTypeFit
 
 
-def evalTable(selected_courses,day_lengths):
+term = "201901"
 
-    tables = [Table()] #free weekby default
 
-    #Acts as buffer for new tables made in each type for each section
-    new_tables={}
+def eval_table(selected_courses, day_lengths):
+
+    tables = [Table()]  # free weekby default
+
+    # Acts as buffer for new tables made in each type for each section
+    new_tables = {}
 
     for course in selected_courses:
         for type_ in course:
             for section in type_:
 
                 # skip if class falls out of required schedule
-                if not check_section_with_day_lengths(section,day_lengths): continue
+                if not check_section_with_day_lengths(section, day_lengths):
+                    continue
 
-                for i,t in enumerate(tables):
-
-
+                for i, t in enumerate(tables):
 
                     # i is in new_tables[(time,section.days,i)] so that a section does keep puting itself
                     #  eg 'A01' in next iterations of tables sees its previous entry in new tables and puts
                     #      itself wih it again.
                     # But it allows other section with same day and time to find the table
 
-                    #[start,end] eg [11,13.5] for'11am-1:20pm'
+                    # [start,end] eg [11,13.5] for'11am-1:20pm'
                     time = section.time
 
-                    if not (time,section.days,i) in new_tables:
+                    if not (time, section.days, i) in new_tables:
                         table = deepcopy(t)
 
                         try:
-                            table = insert_in_table(section,table)
+                            table = insert_in_table(section, table)
 
-                            new_tables[(time,section.days,i)]= table
+                            new_tables[(time, section.days, i)] = table
 
                         except NotFit:
                             continue
                     else:
-                        new_tables[(time,section.days,i)] = insert_same_in_table(section,new_tables[(time,section.days,i)])
+                        new_tables[(time, section.days, i)] = insert_same_in_table(
+                            section, new_tables[(time, section.days, i)]
+                        )
 
             # if no section of a type can be added then
-            if  (not new_tables) and type_: raise NoSectionOfTypeFit(course)
+            if (not new_tables) and type_:
+                raise NoSectionOfTypeFit(course)
 
             # If at start a type_ is [] it makes table [] in comprehension
             # but you want it to remain a free table (default value) so loop
@@ -63,47 +65,53 @@ def evalTable(selected_courses,day_lengths):
 
     return tables
 
-'''
-return True if sectionis compatible with day_lengths
-'''
-def check_section_with_day_lengths(section,day_lengths):
+
+def check_section_with_day_lengths(section, day_lengths):
+    '''
+    returns True if sectionis compatible with day_lengths
+    '''
     for day in section.days:
-        if section.time[0] < day_lengths[day][0] or section.time[1] > day_lengths[day][1]:
+        if (
+            section.time[0] < day_lengths[day][0]
+            or section.time[1] > day_lengths[day][1]
+        ):
             return False
     else:
         return True
 
-'''
-Checks timings for section and inserts them table.
-Raises NotFit Error if any class on a day cannot fit in.
 
-inserts - [start,end,course+courseno.,section]
-eg. [12.5,13.5,'MATH101','T01']
-'''
-def insert_in_table(section,table):
-    time=section.time
+def insert_in_table(section, table):
+    '''
+    Checks timings for section and inserts them table.
+    Raises NotFit Error if any class on a day cannot fit in.
 
-    #Waring this makes the lists in all days point to sam list
-    #if you make change in one to_insert in one day, all of them change
-    to_insert = [time[0],time[1],section.cName+section.cNum,section.section]
+    inserts - [start,end,course+courseno.,section]
+    eg. [12.5,13.5,'MATH101','T01']
+    '''
+
+    time = section.time
+
+    # Waring this makes the lists in all days point to sam list
+    # if you make change in one to_insert in one day, all of them change
+    to_insert = [time[0], time[1], section.cName + section.cNum, section.section]
 
     for day in section.days:
-        #inserting in free day
-        if table[day]==[]:
-            table[day].insert(0,to_insert)
+        # inserting in free day
+        if table[day] == []:
+            table[day].insert(0, to_insert)
 
-        #inserting at start of day
+        # inserting at start of day
         elif table[day][0][0] >= time[1]:
-            table[day].insert(0,to_insert)
+            table[day].insert(0, to_insert)
 
         else:
-            #inserting during the day
-            for i in range(len(table[day])-1):
-                if table[day][i][1] <= time[0] and table[day][i+1][0] >= time[1]:
-                    table[day].insert(i+1,to_insert)
+            # inserting during the day
+            for i in range(len(table[day]) - 1):
+                if table[day][i][1] <= time[0] and table[day][i + 1][0] >= time[1]:
+                    table[day].insert(i + 1, to_insert)
                     break
 
-            #inserting at end of day
+            # inserting at end of day
             else:
                 if table[day][-1][1] <= time[0]:
                     table[day].append(to_insert)
@@ -114,11 +122,12 @@ def insert_in_table(section,table):
 
     return table
 
-'''
-Inserts in a section which has same timings as another inserted class
-eg. [12.5,13.5,'MATH101','T01'] to [12.5,13.5,'MATH101','T01','T02']
-'''
-def insert_same_in_table(section,table):
+
+def insert_same_in_table(section, table):
+    '''
+    Inserts in a section which has same timings as another inserted class
+    eg. [12.5,13.5,'MATH101','T01'] to [12.5,13.5,'MATH101','T01','T02']
+    '''
     time = section.time
     day = section.days[0]
 
@@ -137,51 +146,53 @@ def insert_same_in_table(section,table):
     return table
 
 
-
-
 ######################################################### Main ###############################
 
-'''
-set_term
-takes - input argument for term and
-return - term value equal accordingly
-'''
+
 def set_term(inp):
-    if ('next' in inp) or ('n' in inp):
+    '''
+    set_term
+    takes - input argument for term and
+    return - term value equal accordingly
+    '''
+
+    if ("next" in inp) or ("n" in inp):
 
         # gives tuple (year,month,date,h,min,s,weekday, etc.)
         time = localtime()
 
-        if time[1] in [1,2,3,4]: # next term from may
-            term = str(time[0])+ '05'
+        if time[1] in [1, 2, 3, 4]:  # next term from may
+            term = str(time[0]) + "05"
 
-        elif time[1] in [5,6,7,8]: # next term from sept
-            term = str(time[0])+ '09'
+        elif time[1] in [5, 6, 7, 8]:  # next term from sept
+            term = str(time[0]) + "09"
 
-        else: #next term from jan
-            term = str(time[0]+1)+ '01'
+        else:  # next term from jan
+            term = str(time[0] + 1) + "01"
 
-    elif ('current' in inp) or ('curr' in inp):
+    elif ("current" in inp) or ("curr" in inp):
 
         # gives tuple (year,month,date,h,min,s,weekday, etc.)
         time = localtime()
 
-        if time[1] in [1,2,3,4]: # next term from jan
-            term = str(time[0])+ '01'
+        if time[1] in [1, 2, 3, 4]:  # next term from jan
+            term = str(time[0]) + "01"
 
-        elif time[1] in [5,6,7,8]: # next term from may
-            term = str(time[0])+ '05'
+        elif time[1] in [5, 6, 7, 8]:  # next term from may
+            term = str(time[0]) + "05"
 
-        else: #current term from sept
-            term = str(time[0])+ '09'
+        else:  # current term from sept
+            term = str(time[0]) + "09"
 
     elif inp.isdigit() and len(inp) == 6:
         term = inp
 
     return term
 
+
 ############################## Global Variables
 
-term = set_term('curr')# term chosen
+term = set_term("curr")  # term chosen
 selected_courses = []  # Course objects
-day_lengths = {'M':[0,24],'T':[0,24],'W':[0,24],'R':[0,24],'F':[0,24]}
+day_lengths = {"M": [0, 24], "T": [0, 24], "W": [0, 24], "R": [0, 24], "F": [0, 24]}
+
